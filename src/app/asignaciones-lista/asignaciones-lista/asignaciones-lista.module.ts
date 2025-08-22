a90ecb24ed5f7cdba4dc3d1133607cd484f7205a
Miguel E. Cabrera <mecg1994@gmail.com>
2025-08-22 10:43:25 -0600

    lista-asignacion

--- src/app/asignaciones-lista/asignaciones-lista/asignaciones-lista.module.ts
+++ src/app/asignaciones-lista/asignaciones-lista/asignaciones-lista.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AsignacionesListaRoutingModule } from './asignaciones-lista-routing.module';
+import { AsignacionesListaComponent } from './asignaciones-lista/asignaciones-lista.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AsignacionesListaComponent
+  ],
   imports: [
     CommonModule,
     AsignacionesListaRoutingModule
